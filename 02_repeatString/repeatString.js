eebf28558923c7b5e2dea1605b40992ae1cf80fa
Hash <halchemist@tutamail.com>
2023-12-10 14:12:10 +0400

    Feat: Solve exercise 2 - repeatString
    This commit adds the solutions for exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(repeatingString, numberOfTimes) {
+    returnString="";
+    if(numberOfTimes>=0){
+        for(let i = 0; i < numberOfTimes; i++){
+            returnString+=repeatingString;        
+        }
+        return returnString;
+    }
+    else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
