1230e1440fe8d6572b00bd396870e5e325fa092a
Hash <halchemist@tutamail.com>
2023-12-10 14:29:40 +0400

    Refactor: convert string into an array at exercise 2 for one line code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,5 @@
 const reverseString = function(string){
-    let reversedString="";
-    stringLength=string.length-1;
-    
-    for(let i=stringLength;i>-1;i--){
-        reversedString+=string.substr(i,1);
-    }
-    return reversedString;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
