b85f4c788386c727f4c43fedcfcbd8a668b9f426
Hash <halchemist@tutamail.com>
2023-12-10 16:37:14 +0400

    Feat: Solve exercise 5 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(limit1, limit2){
+    let sum=0;
 
-};
+    if((typeof limit1!="number" || typeof limit2!="number") || (limit1<0 || limit2<0)){
+        return "ERROR"
+    }
+
+    if(limit1<limit2)
+    {
+        for(let number=limit1; number<=limit2; number++)
+        {
+            sum+=number;
+        }
+    }
+    else{
+        for(let number=limit1; number>=limit2; number--){
+            sum+=number;
+        }
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
