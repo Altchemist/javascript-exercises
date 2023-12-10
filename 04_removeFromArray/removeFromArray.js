6e13ece9c8aa860590a78d9d4239c4ef152e99e5
Hash <halchemist@tutamail.com>
2023-12-10 16:07:50 +0400

    Feat: Solve exercise 3 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...args) {
+    const newArray=[];
+    let toRemove=false;
 
+    for(element of theArray){
+        toRemove=false;
+        for(const remove of args)
+        {
+            if (element===remove){
+                toRemove=true;
+                break;
+            }            
+        }
+        if(toRemove==false)
+        {
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
