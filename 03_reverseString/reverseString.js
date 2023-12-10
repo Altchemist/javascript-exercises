3d72c92d3b5bbebf3d29ef687516d3719ffa9c30
Hash <halchemist@tutamail.com>
2023-12-10 14:25:15 +0400

    Feat: Solve exercise 2 - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string){
+    let reversedString="";
+    stringLength=string.length-1;
+    
+    for(let i=stringLength;i>-1;i--){
+        reversedString+=string.substr(i,1);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
